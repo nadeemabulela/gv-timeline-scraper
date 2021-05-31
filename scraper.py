dc98e68b2dd0862f39f96032ed438230aeb7b6f4
nadeemabulela <nadeemabulela@gmail.com>
2021-05-31 21:08:08 +0200

    saved data in a seperate file

--- scraper.py
+++ scraper.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Get html code for the url
 html = requests.get('https://populartimelines.com/timeline/Gary-Vaynerchuk').text
 soup = BeautifulSoup(html, 'lxml')
 
-event = soup.find('div', class_ = 'event')
+# Get all div tags with the event class
+events = soup.find_all('div', class_ = 'event')
 
-time = event.find('span', class_ = 'date-wrapper').text
-desc = event.find('p').text
+for event in events:
+    # Extract time and description from the event
+    time = event.find('span', class_ = 'date-wrapper').text
+    desc = event.find('p').text.strip()
 
-print(time)
-print(desc)
+    # Save data in a seperate file
+    with open('results.txt', 'a', encoding='utf-8') as f:
+        f.write(f'{time}\n')
+        f.write(f'{desc}\n')
+        f.write('\n')
+    
+    print('Saved a new record!')
